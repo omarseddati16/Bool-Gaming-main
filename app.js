404a3cc0a5833051387dfccbe5d39083cb4f542e
omarseddati16 <omarseddati16@gmail.com>
2025-10-07 18:15:26 +0200

    [fix]

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
+// Carica dotenv solo in locale
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
 const express = require('express');
 const cors = require('cors');
 
-
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 const routers = require('./routes/routers');
 const notFound = require('./middlewares/routesNotFound');
 const error = require('./middlewares/error');
 
-
-app.use(express.static('public'))
-
+app.use(express.static('public'));
 app.use(cors());
 app.use(express.json());
 
@@ -19,11 +21,11 @@ app.get('/', (req, res) => {
   res.send('Server attivo!');
 });
 
-app.use('/videogames', routers)
+app.use('/videogames', routers);
 
-app.use(notFound)
-app.use(error)
+app.use(notFound);
+app.use(error);
 
 app.listen(port, () => {
-  console.log(`server in ascolto nella porta ${port}`);
+  console.log(`Server in ascolto nella porta ${port}`);
 });
