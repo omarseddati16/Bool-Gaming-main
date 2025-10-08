5af06c15c194bf8cde2829eebc445ecf70e80729
omarseddati16 <omarseddati16@gmail.com>
2025-10-08 12:14:55 +0200

    fix

--- data/database.js
+++ data/database.js
@@ -8,6 +8,7 @@ const connection = mysql.createConnection({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     port: process.env.DB_PORT
+
 });
 
 // stabilisco la connessione al db
